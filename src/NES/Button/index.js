7581b3e89a10eadfb54d6539ad0a7040aa1de909
Adarsh ML <mladarsh08@gmail.com>
2020-07-08 23:39:07 +0530

    Add React Style

--- src/NES/Button/index.js
+++ src/NES/Button/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import './nes-button.css';
 
+const normalButton = {
+    bottom: 'black',
+    shadow: 'rgb(173, 175, 188)',
+    top:'white',
+    selected:'rgb(231, 231, 231)'
+};
+
 
 
 function NESButton() {
   return (
-    <div class="button-normal">
-      <div class="black-fat" />
-      <div class="black-tall" />
-      <div class="middle">
-        <div class="top" />
+    <div class="button-normal" >
+      <div class="black-fat" style={normalButton.bottom}/>
+      <div class="black-tall" style={normalButton.bottom}/>
+      <div class="middle" style={normalButton.shadow}>
+        <div class="top" style={normalButton.top}/>
       </div>
       <p>feanor</p>
     </div>
